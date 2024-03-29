c2765f34feddd8b539fd8d99f9296729bdaddd43
Vasco Lusitano <vasco.lusitano@kuehne-nagel.com>
2024-03-29 22:42:03 +0000

    flyway configurations change and documentation added

--- backend/src/main/resources/db/migration/local/V3__RoleOperations.sql
+++ backend/src/main/resources/db/migration/local/V3__RoleOperations.sql
@@ -1,2 +1,5 @@
 INSERT INTO role (name) values ('Vasco');
 INSERT INTO role (name) values ('Vitor');
+INSERT INTO role (name) values ('Josue');
+INSERT INTO role (name) values ('Frank');
+INSERT INTO role (name) values ('Gerson');
