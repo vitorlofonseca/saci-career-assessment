aa752b2d36275dccbe0cd7b830345292ebf3a437
Vasco Lusitano <vasco.lusitano@kuehne-nagel.com>
2024-03-30 22:28:03 +0000

    fix

--- backend/src/main/java/saci/domain/model/Role.java
+++ backend/src/main/java/saci/domain/model/Role.java
@@ -1,18 +1,21 @@
 package saci.domain.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "role")
 public class Role {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 }
