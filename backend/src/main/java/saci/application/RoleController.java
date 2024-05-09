1fb2f9732e7f9d03df91b43098ed4f54e2a080ad
silva-00 <martimandre.ms@gmail.com>
2024-05-09 09:32:12 +0100

    Action button to edit a role (small changes)

--- backend/src/main/java/saci/application/RoleController.java
+++ backend/src/main/java/saci/application/RoleController.java
@@ -69,8 +69,6 @@ public class RoleController {
         List<Role> roles = roleService.getRoles();
         return roles.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(roles);
     }
-
-    @PutMapping("/{roleId}")
     @Operation(summary = "Edit a role", description = "Edit an existing role with the provided role ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Role edited successfully"),
@@ -78,6 +76,8 @@ public class RoleController {
             @ApiResponse(responseCode = "404", description = "Role not found"),
             @ApiResponse(responseCode = "500", description = "Internal server error")
     })
+
+    @PutMapping("/{roleId}")
     public ResponseEntity<Role> editRole(@PathVariable Long roleId, @Valid @RequestBody Role updatedRole) {
         Role editedRole = roleService.editRole(roleId, updatedRole);
         return ResponseEntity.ok(editedRole);
