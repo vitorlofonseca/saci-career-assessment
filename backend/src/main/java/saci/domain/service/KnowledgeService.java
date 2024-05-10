d6589c9d0a2616edb7ed80043ab06df27a88d16c
Pedro <pmagalhaes489@gmail.com>
2024-05-10 11:24:29 +0100

    PR fixes

--- backend/src/main/java/saci/domain/service/KnowledgeService.java
+++ backend/src/main/java/saci/domain/service/KnowledgeService.java
@@ -9,6 +9,7 @@ import saci.domain.service.exceptions.AlreadyExistsException;
 import saci.infrastructure.KnowledgeRepository;
 
 import java.util.Optional; // Import Optional if it's not already imported
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 import saci.domain.model.Knowledge;
 import saci.infrastructure.KnowledgeRepository;
