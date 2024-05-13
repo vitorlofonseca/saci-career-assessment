7470048e991761a506c484627a5f4fbca73f9963
fromgonie <166744837+fromgonie@users.noreply.github.com>
2024-05-13 14:09:53 +0100

    Develop Table to View Knowledges (Update #1)

--- frontend/src/views/RoleView.vue
+++ frontend/src/views/RoleView.vue
@@ -1,82 +1,7 @@
-<script lang="ts" setup>
-import { ElButton, ElDialog, ElInput, ElTable, ElTableColumn } from 'element-plus'
-import { useKnowledgeStore } from '@/stores/knowledges/index'
-import { ErrorMessage, SuccessMessage } from '@/services/messages/messages'
-import { ref, onMounted } from 'vue'
-import type { Knowledge } from '@/domain/Knowledge'
-import { useRolesStore } from '@/stores/roles/index'
-
-const dialogFormVisible = ref(false)
-const knowledgesStore = useKnowledgeStore()
-const newKnowledgeName = ref('')
-const roleStore = useRolesStore()
-const roleId = ref<number>(1)
-const roleName = ref<string>('')
-const knowledges = ref<Knowledge[]>([])
-
-onMounted(async () => {
-  try {
-    await Promise.all([knowledgesStore.fetchKnowledges(), roleStore.fetchRoles()])
-    const role = roleStore.roles.find((role) => role.id === roleId.value)
-    if (role) {
-      roleName.value = role.name
-      console.log('Nome da Role:' + roleName.value)
-      // Filtrando os conhecimentos com roleId correspondente
-      knowledges.value = knowledgesStore.knowledges.filter(
-        (knowledge) => parseInt(knowledge.roleId) === roleId.value
-      )
-    } else {
-      console.error('Papel não encontrado com o ID fornecido.')
-    }
-  } catch (error) {
-    console.error('Erro ao buscar conhecimentos ou papéis:', error)
-  }
-})
-
-const handleDetail = (row: Knowledge) => {
-  console.log('Detail clicked for:', row)
-}
-const handleEdit = (row: Knowledge) => {
-  console.log('Edit clicked for:', row)
-}
-
-const showDialog = () => {
-  dialogFormVisible.value = true
-}
-
-const hideDialog = () => {
-  dialogFormVisible.value = false
-}
-
-const createKnowledge = async () => {
-  if (newKnowledgeName.value == '') {
-    ErrorMessage('You need to fill in this field')
-    showDialog()
-    return
-  }
-
-  try {
-    await knowledgesStore.saveKnowledge({
-      name: newKnowledgeName.value,
-      roleId: '1',
-      levelId: '',
-      weight: '1'
-    })
-    SuccessMessage('Your knowledge was created')
-    hideDialog()
-  } catch (error: any) {
-    ErrorMessage('An error occurred while creating the knowledge')
-  }
-
-  newKnowledgeName.value = ''
-}
-</script>
-
 <template>
   <div class="PageWrapper">
     <div class="TableContainer">
       <h1>{{ roleName }}</h1>
-      <br />
       <h2>Attached Knowledges</h2>
       <br />
       <ElTable :data="knowledges" style="width: 100%">
@@ -91,37 +16,67 @@ const createKnowledge = async () => {
         </ElTableColumn>
       </ElTable>
     </div>
-    <br />
-    <div class="newKnowledge">
-      <ElButton type="primary" @click="showDialog"> Create Knowledge </ElButton>
-      <ElDialog v-model="dialogFormVisible" title="Knowledge" width="500">
-        <ElInput v-model="newKnowledgeName" placeholder="Knowledge name" />
-
-        <template #footer>
-          <div class="dialog-footer">
-            <ElButton @click="hideDialog">Cancel</ElButton>
-            <ElButton type="primary" @click="createKnowledge"> Confirm </ElButton>
-          </div>
-        </template>
-      </ElDialog>
-    </div>
   </div>
 </template>
-<style lang="scss" scoped>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { ElTable, ElTableColumn, ElButton } from 'element-plus'
+import type { Knowledge } from '@/domain/Knowledge'
+import { useKnowledgeStore } from '@/stores/knowledges/index'
+import { useRolesStore } from '@/stores/roles/index'
+
+const handleDetail = (row: Knowledge) => {
+  console.log('Detail clicked for:', row)
+}
+
+const handleEdit = (row: Knowledge) => {
+  console.log('Edit clicked for:', row)
+}
+
+const knowledgesStore = useKnowledgeStore()
+const roleStore = useRolesStore()
+
+const roleId = ref<number>(1)
+const roleName = ref<string>('')
+const knowledges = ref<Knowledge[]>([])
+
+onMounted(async () => {
+  try {
+    await Promise.all([knowledgesStore.fetchKnowledges(), roleStore.fetchRoles()])
+
+    const role = roleStore.roles.find((role) => role.id === roleId.value)
+
+    if (role) {
+      roleName.value = role.name
+      console.log('Nome da Role:' + roleName.value)
+
+      // Filtrando os conhecimentos com roleId correspondente
+      knowledges.value = knowledgesStore.knowledges.filter(
+        (knowledge) => parseInt(knowledge.roleId) === roleId.value
+      )
+    } else {
+      console.error('Papel não encontrado com o ID fornecido.')
+    }
+  } catch (error) {
+    console.error('Erro ao buscar conhecimentos ou papéis:', error)
+  }
+})
+</script>
+
+<style scoped lang="scss">
 .PageWrapper {
-  padding: 100px;
-}
-
-.newKnowledge {
-  text-align: right;
-}
-
-.newRole {
-  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 20px;
+  height: 100vh;
+}
+
+.TableContainer {
+  width: 800px;
+  height: auto;
+  margin: auto;
+  text-align: left;
 }
 </style>
