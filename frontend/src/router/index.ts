db8f0eb637227b689b364a7213243ba6591a1aa7
Pedro <pmagalhaes489@gmail.com>
2024-05-13 11:50:20 +0100

    Knowledge button(add knowledge)

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ReferencesSection from '../views/ReferencesSection.vue'
 import PadawanView from '@/views/PadawanView.vue'
 import HomeView from '@/views/HomeView.vue'
-import DeleteViewTemp from '@/views/DeleteViewTemp.vue'
+import knowledgebutton from '@/views/knowledgebutton.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,9 +16,14 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/delete',
-      component: DeleteViewTemp
+      path: '/',
+      redirect: '/knowledgebutton'
     },
+    {
+      path: '/knowledgebutton',
+      component: knowledgebutton
+    },
+
     {
       path: '/padawan',
       name: 'padawan',
