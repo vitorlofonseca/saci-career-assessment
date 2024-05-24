f23dc95431907d4e016fbbd79ed62a0e189f0067
silva-00 <martimandre.ms@gmail.com>
2024-05-24 15:03:52 +0100

    tables (fix)

--- backend/src/main/resources/db/migration/prod/V2__KnowledgeStructural.sql
+++ backend/src/main/resources/db/migration/prod/V2__KnowledgeStructural.sql
@@ -1,8 +1,6 @@
 CREATE TABLE knowledge (
     id INT AUTO_INCREMENT PRIMARY KEY,
     role_id INT,
-    level_id INT,
     name VARCHAR(255),
-    weight INT,
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
