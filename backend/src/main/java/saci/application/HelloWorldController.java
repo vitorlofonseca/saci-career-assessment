1664a08d8585a28f7a6fa52d239c80fb02bf28bf
Pedro <pmagalhaes489@gmail.com>
2024-05-29 14:38:31 +0100

    Backend(POST,GET)

--- backend/src/main/java/saci/application/HelloWorldController.java
+++ backend/src/main/java/saci/application/HelloWorldController.java
@@ -1,6 +1,9 @@
 package saci.application;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,11 @@ public class HelloWorldController {
     @GetMapping
     public String hello() {
         return "Hello world";
+    }
+
+    @PostMapping
+    public ResponseEntity<String> helloPost(@RequestBody String requestBody) {
+        // Process the POST request
+        return ResponseEntity.ok("Received POST request with body: " + requestBody);
     }
 }
