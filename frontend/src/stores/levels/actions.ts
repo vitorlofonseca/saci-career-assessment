1c82f5ff5a7d3c4204c7407b50e14ca6021f67c6
Pedro <pmagalhaes489@gmail.com>
2024-06-17 10:56:46 +0100

    PR

--- frontend/src/stores/levels/actions.ts
+++ frontend/src/stores/levels/actions.ts
@@ -2,20 +2,16 @@ import { levels } from './state'
 import { get } from '@/services/http'
 
 export async function fetchLevelsByRoleId(roleId: string) {
-  try {
-    const data: any[] | string = await get(`/levels?role_id=${roleId}`)
-    if (Array.isArray(data)) {
-      levels.value = data.map((level: any) => ({
-        id: level.id,
-        name: level.name,
-        minCoefficient: 0,
-        maxCoefficient: 0,
-        link: ''
-      }))
-    } else {
-      console.error('Failed to fetch levels: Response is not an array:', data)
-    }
-  } catch (error) {
-    console.error('Failed to fetch levels:', error)
+  const data: any[] | string = await get(`/levels?role_id=${roleId}`)
+  if (Array.isArray(data)) {
+    levels.value = data.map((level: any) => ({
+      id: level.id,
+      name: level.name,
+      minCoefficient: 0,
+      maxCoefficient: 0,
+      link: ''
+    }))
+  } else {
+    throw new Error('Failed to fetch levels: Response is not an array.')
   }
 }
