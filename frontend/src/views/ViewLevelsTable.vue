f28bf31595aa7db07a473cf34bc78397c3a6fdd6
Pedro <pmagalhaes489@gmail.com>
2024-06-20 09:59:31 +0100

    PR final

--- frontend/src/views/ViewLevelsTable.vue
+++ frontend/src/views/ViewLevelsTable.vue
@@ -4,67 +4,25 @@
       <h1>{{ role?.name }}</h1>
       <h2>Attached Levels</h2>
       <br />
-      <ElTable :data="sortedLevels" style="width: 100%">
+      <ElTable :data="role?.levels" style="width: 100%">
         <ElTableColumn prop="name" label="Levels" />
-        <ElTableColumn fixed="right" label="Actions" width="150">
-          <template #default="{ row }">
-            <div>
-              <ElButton @click="onDeleteRow(row)" type="text" size="small">Delete</ElButton>
-              <ElButton @click="onEditRow(row)" type="text" size="small">Edit</ElButton>
-            </div>
-          </template>
-        </ElTableColumn>
       </ElTable>
-
-      <div class="NewLevel">
-        <ElButton type="primary" @click="redirectToLevelView()"> Create Level </ElButton>
-      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { ElTable, ElTableColumn, ElButton } from 'element-plus'
-import type { Level } from '@/domain/Level'
+import { ElTable, ElTableColumn } from 'element-plus'
 import { useRolesStore } from '@/stores/roles/index'
-import { useLevelStore } from '@/stores/levels'
 import type { Role } from '@/domain/Role'
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-const redirectToLevelView = () => {
-  router.push('/level-view')
-}
-
-const onDeleteRow = (row: Level) => {
-  console.log('Delete clicked for:', row)
-}
-
-const onEditRow = (row: Level) => {
-  console.log('Edit clicked for:', row)
-}
 
 const roleStore = useRolesStore()
 const role = ref<Role>()
-const levels = ref<Level[]>([])
-const levelsStore = useLevelStore()
-
-const sortLevels = (levels: Level[]): Level[] => {
-  return levels.slice().sort((a, b) => {
-    return a.minCoefficient - b.minCoefficient || a.maxCoefficient - b.maxCoefficient
-  })
-}
-
-const sortedLevels = ref<Level[]>([])
+const roleId = ref('1')
 
 onMounted(async () => {
-  role.value = await roleStore.getRoleById('1')
-  if (role.value) {
-    levels.value = role.value.levels
-    sortedLevels.value = sortLevels(levels.value)
-  }
+  role.value = await roleStore.getRoleById(roleId.value)
 })
 </script>
 
