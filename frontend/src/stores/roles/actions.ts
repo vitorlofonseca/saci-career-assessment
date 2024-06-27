ef05dfed48057e78d84c662e8ccb771a75da2276
Pedro <pmagalhaes489@gmail.com>
2024-06-27 14:47:34 +0100

    PR

--- frontend/src/stores/roles/actions.ts
+++ frontend/src/stores/roles/actions.ts
@@ -2,7 +2,7 @@ import { get, post, put } from '@/services/http'
 import { getRoles } from './getters'
 import type { Role } from '@/domain/Role'
 import { deleteRequest } from '@/services/http'
-import { roles } from './state'
+import { roles, levels } from './state'
 import type { Level } from '@/domain/Level'
 
 function setRoles(newRoles: Role[]): void {
@@ -38,9 +38,10 @@ async function removeRole(roleId: string) {
   await deleteRequest(`/roles/${roleId}`)
   roles.value = roles.value.filter((role) => role.id !== parseInt(roleId))
 }
-async function removeLevel(levelId: number) {
-  const response = await deleteRequest(`/levels/${levelId}`)
-  return response
+function removeLevel(levelId: number): Promise<void> {
+  return deleteRequest(`/levels/${levelId}`).then(() => {
+    levels.value = levels.value.filter((level) => level.id !== levelId)
+  })
 }
 
 async function addLevel(level: Level, role: Role): Promise<void> {
--- frontend/src/stores/roles/state.ts
+++ frontend/src/stores/roles/state.ts
@@ -1,6 +1,11 @@
 import type { Role } from '@/domain/Role'
+import type { Level } from '@/domain/Level'
 import { type Ref, ref } from 'vue'
 
 const roles: Ref<Role[]> = ref([])
 
 export { roles }
+
+const levels: Ref<Level[]> = ref([])
+
+export { levels }
